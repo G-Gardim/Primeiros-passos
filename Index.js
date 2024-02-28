6db027479f467bfaeb35f93cdd6a4907492f954e
G-Gardim <gustavo1999gardim@gmail.com>
2024-02-28 20:49:21 -0300

    Refazendo

---
+++ Index.js
@@ -0,0 +1,24 @@
+let timePokemon = [
+    ['Poochyena ', 'M', 2, 13 / 13],
+    ['Zigzagoon ', 'F', 2, 13 / 13],
+    ['Dragonite ', 'M', 5, 25 / 25],
+    ['Dragonite ', 'F', 5, 24 / 24],
+    ['Dragonite ', 'F', 5, 24 / 24] , 
+    ['Poochyena ', 'F', 3, 15 / 15],
+    ['Wurmple ', 'M', 2, 7 / 14],
+  ]
+  
+  console.log(timePokemon[0][0] + 'cadastrado com sucesso. ')
+  
+  console.log(timePokemon[1][0] + 'cadastrado com sucesso.')
+  
+  console.log(timePokemon[2][0] + 'cadastrado com sucesso.')
+  
+  console.log(timePokemon[3][0] + 'cadastrado com sucesso.')
+  
+  console.log(timePokemon[4][0] + 'cadastrado com sucesso.')
+  
+  console.log(timePokemon[5][0] + 'cadastrado com sucesso.')
+  
+  console.log(timePokemon[6][0] + 'cadastrado com sucesso.')
+  
